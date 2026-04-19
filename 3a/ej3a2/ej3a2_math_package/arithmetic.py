2cd8f887ca00fa356afa27ced95b8930686d741b
Iván Idígoras <ivan.idigoras@outlook.com>
2026-04-19 23:15:54 +0200

    ejercicios resueltos

--- 3a/ej3a2/ej3a2_math_package/arithmetic.py
+++ 3a/ej3a2/ej3a2_math_package/arithmetic.py
@@ -1,12 +1,11 @@
 # arithmetic.py
+
 import math
 
 def power(base: float, exponent: float) -> float:
 	# Write your code here
-	pass
+	return math.pow(base, exponent)
 
 def square_root(num_1: float) -> float:
 	# Write your code here
-	pass
-
-
+	return math.sqrt(num_1)
--- 3a/ej3a3/ej3a3_time_package/__init__.py
+++ 3a/ej3a3/ej3a3_time_package/__init__.py
@@ -1,2 +1,7 @@
 # write import for convert_date_format
+from . import convert_date_format
+
 # write import for date_operations
+from . import date_operations
+
+__all__ = ["convert_date_format", "date_operations"]
